1b1f7d1c1ca1a3c12f19cf9e2c27b5a411254d33
Minh Huynh <mhuynh09@gmail.com>
2019-12-25 19:52:50 +1100

    Changed spacing

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -4,15 +4,15 @@ const dbUrl = process.env.DB_URL || "mongodb://localhost:27017";
 const dbName = process.env.DB_NAME || "test";
 
 async function connect() {
-	try {
-		await mongoose.connect(`${dbUrl}/${dbName}`, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useFindAndModify: false
-		});
-	} catch (err) {
-		console.log("[ERROR][dbConnect] Db connection failed");
-	}
+  try {
+    await mongoose.connect(`${dbUrl}/${dbName}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+  } catch (err) {
+    console.log("[ERROR][dbConnect] Db connection failed");
+  }
 }
 
 module.exports = connect;
